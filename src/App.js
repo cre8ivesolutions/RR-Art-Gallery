9628666d6125a885878f328bb847f1d6ba70e1af
cre8ivesolutions <tammerz78@gmail.com>
2022-09-10 09:47:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  // your logic goes here!
+  // your logic goes here...
 
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
         <button onClick={() => {}}>Clear</button>
         <button onClick={() => {}}>Next</button>
         <button onClick={() => {}}>Back</button>
+
       </div>
       <input onChange={(e) => { }} />
       <div>
